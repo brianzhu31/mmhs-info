5329a944c1f660fa95d4af8ff2ac843c7126cd3a
Brian Zhu <brianzhu4321@gmail.com>
2022-12-30 20:40:56 -0500

    added mmhs header with fade in transition

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './home.css';
 
 export default function Home() {
   return (
     <>
-      <div className='home-container'>
-        asddsadasdas
+      <div className='home'>
+        <div className='heading-container'>
+          <div className='school-name-text-container'>
+            <h1>Milliken Mills</h1>
+            <h2>High School</h2>
+          </div>
+        </div>
       </div>
     </>
   )
