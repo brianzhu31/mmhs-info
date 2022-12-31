911a8d5fcb4e1668b5a50c87042fc090307bba32
Brian Zhu <brianzhu4321@gmail.com>
2022-12-30 20:39:09 -0500

    gave navbar full functionality, fixed up styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 
-export default function Navbar() {
+export default function Navbar(props) {
+
+  function checkMenu() { 
+    if(props.menuOn === false) { // first checks if the menu is on or off, then toggles the opposite
+      props.toggleMenuIcon('Close')
+      props.toggleMenu(true) 
+    } // props.toggleMenu() was passed down from parent component App.js, it can change the state of menuOn in App.js
+    else {
+      props.toggleMenuIcon('Menu')
+      props.toggleMenu(false)
+    }
+  }
+
+  function closeMenu() {
+    if(props.menuOn === true) {
+      props.toggleMenuIcon('Menu')
+      props.toggleMenu(false)
+    }
+  }
+
   return (
 
-    <div className='navbar'>
+    <nav className='navbar'>
       <div className='navbar-container'>
-        <Link to='/' className='navbar-logo'>
-          MMHS
+        <Link to='/' className='navbar-logo' onClick={closeMenu}> {/* onClick function calls closeMenu above. Ensures that menu is closed when click on logo to return home. */}
+          LOGO
         </Link>
-        <div className='menu-icon'>
-          Menu
+        <div className='menu-icon' onClick={checkMenu}> {/* onClick function calls checkMenu above */}
+          {props.menuIcon}
         </div>
       </div>
-    </div>
+    </nav>
 
   )
 }
